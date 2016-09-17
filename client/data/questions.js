8f69bb25753a1c088934d5985c8669d337545456
wentran <wen.tran1@gmail.com>
2016-09-17 15:54:28 -0400

    initial form setup in survey.js

---
+++ client/data/questions.js
@@ -0,0 +1,20 @@
+const questions = [
+ {"intro": "Let's get to know about you"}, 
+  
+// let's get straight to the point
+{"price": "What is your price range ?"},
+
+// location /neighborhood
+
+{"location": "Tell me a little bit about where you want to live and ra"}
+ 
+
+
+// let's make it simple for you
+
+ // location/neighborhood
+ {}
+
+]
+
+export default questions;
