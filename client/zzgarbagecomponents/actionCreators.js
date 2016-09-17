07beb91ec10adc2b7041e40e7dac9dc99856db1a
samgowda <samgowda@gmail.com>
2016-09-17 09:27:17 -0400

    basic skeleton

---
+++ client/zzgarbagecomponents/actionCreators.js
@@ -0,0 +1,27 @@
+// increment
+export function increment(index){
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+// add comments
+
+export function addComment(postId, author, comment){
+  return {
+    type: 'ADD_COMMENT'
+    postId,
+    author,
+    comment
+  }
+}
+
+export function removeComment(postId, i){
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
+
+//remove comment
