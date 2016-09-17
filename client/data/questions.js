b1e2bfff3f54af0d60326622e3472b1ba78b9d08
wentran <wen.tran1@gmail.com>
2016-09-17 16:41:38 -0400

    draft of questions

--- client/data/questions.js
+++ client/data/questions.js
@@ -1,20 +1,29 @@
 const questions = [
- {"intro": "Let's get to know about you"}, 
+ {"intro": "Let's get to know a little bit about you"}, 
   
-// let's get straight to the point
-{"price": "What is your price range ?"},
+//**Rent: let's get straight to the point
+{"price": "What is your price range ?"},  
+    // price range selection pop-up
 
-// location /neighborhood
+// **location /neighborhood
+{"location": "Let's take a look at where you want to live"},
+    // list of neighorhood pop-up /drop down for users to select
 
-{"location": "Tell me a little bit about where you want to live and ra"}
- 
 
+ {"roomNumber": "How many bedrooms?"},
+    // list of room types: studio,one-bedroom, 2-bedroom, etc..
+      
+   //Time is of the essence. how long of a commute are you looking at?
+  {"commute": "range of commute time"}
 
-// let's make it simple for you
+    //Building Amenities:
+      // There are a few other things...let's make it simple for you. 
+      // Select the amenity that you want to have ?
+  {"Amenities": "modal of Amenities for users to selec"},
 
- // location/neighborhood
- {}
-
+    //Pets are our best friends. Let us get to know if you have any
+  {"pets": "list of pets"}
+      // 
 ]
 
 export default questions;
