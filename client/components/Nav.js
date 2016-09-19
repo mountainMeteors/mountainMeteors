d572b166cd85e01b2ab2f6ea80f5948f8d0b281e
wentran <wen.tran1@gmail.com>
2016-09-18 22:58:55 -0400

    nav bar finally working

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,29 +1,22 @@
 import React from 'react';
-var {Link, IndexLink} = require('react-router');
-
-const Nav = () => {
-  return (
-    <div>
-
-      // <Link to="/about" className="about">Get Started</Link>
-      // <Link to="/examples" className="listings">page 2</Link>
-
-<nav class="navbar navbar-light bg-faded">
-  <ul class="nav navbar-nav">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Features</a>
-    </li>
-     </ul>
-</nav>
-
-      
-    </div>
-  );
-};
-
-module.exports = Nav;
-
-
+import AppBar from 'material-ui/AppBar';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+export class Nav extends React.Component {
+  getChildContext() {
+    return { muiTheme: getMuiTheme(baseTheme) };
+  }
+  render() {
+    return (
+      <div>
+      <AppBar
+      title="Menu"
+      />
+      </div>
+      );
+    }        
+  }
+  Nav.childContextTypes = {
+    muiTheme: React.PropTypes.object.isRequired,
+  };
+  export default Nav;
