5a0d0cc64c6625e33dc804ff2489c85dd1e615c8
wentran <wen.tran1@gmail.com>
2016-09-19 19:35:59 -0400

    questions added and working

--- client/components/DropdownMenu.js
+++ client/components/DropdownMenu.js
@@ -8,39 +8,39 @@ const styles = {
   },
 };
 
-export default class DropdownMenu extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {value: 1};
-  }
-
-  handleChange = (event, index, value) => this.setState({value});
-
-  render() {
-    return (
-      <div>
-        <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-          <MenuItem value={1} primaryText="Never" />
-          <MenuItem value={2} primaryText="Every Night" />
-          <MenuItem value={3} primaryText="Weeknights" />
-          <MenuItem value={4} primaryText="Weekends" />
-          <MenuItem value={5} primaryText="Weekly" />
-        </DropDownMenu>
-        <br />
-        <DropDownMenu
-          value={this.state.value}
-          onChange={this.handleChange}
-          style={styles.customWidth}
-          autoWidth={false}
-        >
-          <MenuItem value={1} primaryText="Custom width" />
-          <MenuItem value={2} primaryText="Every Night" />
-          <MenuItem value={3} primaryText="Weeknights" />
-          <MenuItem value={4} primaryText="Weekends" />
-          <MenuItem value={5} primaryText="Weekly" />
-        </DropDownMenu>
-      </div>
-    );
-  }
-}
+// export default class DropdownMenu extends React.Component {
+//
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: 1};
+//   }
+//
+//   handleChange = (event, index, value) => this.setState({value});
+//
+//   render() {
+//     return (
+//       <div>
+//         <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+//           <MenuItem value={1} primaryText="Never" />
+//           <MenuItem value={2} primaryText="Every Night" />
+//           <MenuItem value={3} primaryText="Weeknights" />
+//           <MenuItem value={4} primaryText="Weekends" />
+//           <MenuItem value={5} primaryText="Weekly" />
+//         </DropDownMenu>
+//         <br />
+//         <DropDownMenu
+//           value={this.state.value}
+//           onChange={this.handleChange}
+//           style={styles.customWidth}
+//           autoWidth={false}
+//         >
+//           <MenuItem value={1} primaryText="Custom width" />
+//           <MenuItem value={2} primaryText="Every Night" />
+//           <MenuItem value={3} primaryText="Weeknights" />
+//           <MenuItem value={4} primaryText="Weekends" />
+//           <MenuItem value={5} primaryText="Weekly" />
+//         </DropDownMenu>
+//       </div>
+//     );
+//   }
+// }
--- client/components/Grid.js
+++ client/components/Grid.js
@@ -27,32 +27,32 @@ const tilesData = [
   {
     img: "client/assets/Piggy-bank-icon.png",
     title: 'Budget',
-    question: "What is your price range ? ",
+    status: "What is your price range ? ",
   },
   {
     img: 'images/grid-list/burger-827309_640.jpg',
     title: 'Location',
-    question: "Let's take a look at where you want to live ?" ,
+    status: "Let's take a look at where you want to live ?" ,
   },
   {
     img: 'images/grid-list/camera-813814_640.jpg',
     title: 'Commute',
-    question: "Time is of the essence. how long of a commute are you looking at? ",
+    status: "Time is of the essence. how long of a commute are you looking at? ",
   },
   {
     img: 'images/grid-list/morning-819362_640.jpg',
     title: 'The basics',
-    question: "How many bedrooms?",
+    status: "How many bedrooms?",
   },
   {
     img: 'images/grid-list/hats-829509_640.jpg',
     title: 'Extra Amenities',
-    question: "There are a few other things...let's make it simple for you",
+    status: "There are a few other things...let's make it simple for you",
   },
   {
     img: 'images/grid-list/honey-823614_640.jpg',
     title: 'Pets',
-    question: " Pets are our best friends. Let us get to know if you have any little furry friends",
+    status: " Pets are our best friends. Let us get to know if you have any little furry friends",
   },
  ];
 
@@ -60,20 +60,22 @@ const tilesData = [
  export class Grid extends React.Component {
    getChildContext() {
      return { muiTheme: getMuiTheme(baseTheme) };
-   }
+   };
+
+
    render() {
      return (
   <div style={styles.root}>
     <GridList
-      cellHeight={200}
+      cellHeight={300}
       style={styles.gridList}
     >
-      <Subheader>"  Let's get to know a little bit about you"</Subheader>
+      <Subheader>" Let's get to know a little bit about you"</Subheader>
       {tilesData.map((tile) => (
         <GridTile
           key={tile.img}
           title={tile.title}
-          subtitle={<span> View <b>{tile.status}</b></span>}
+          subtitle={<span><b>{tile.status}</b></span>}
           actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
         >
           <img src={tile.img} />
