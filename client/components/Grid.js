917e949d15aa3d763f58c0f1f370db9c117b144f
wentran <wen.tran1@gmail.com>
2016-09-19 19:09:33 -0400

    questions added

--- client/components/Grid.js
+++ client/components/Grid.js
@@ -27,50 +27,48 @@ const tilesData = [
   {
     img: "client/assets/Piggy-bank-icon.png",
     title: 'Budget',
-    Status: 'jill111',
+    question: "What is your price range ? ",
   },
   {
     img: 'images/grid-list/burger-827309_640.jpg',
     title: 'Location',
-    Status: 'pashminu',
+    question: "Let's take a look at where you want to live ?" ,
   },
   {
     img: 'images/grid-list/camera-813814_640.jpg',
     title: 'Commute',
-    Status: 'Danson67',
+    question: "Time is of the essence. how long of a commute are you looking at? ",
   },
   {
     img: 'images/grid-list/morning-819362_640.jpg',
     title: 'The basics',
-    Status: 'fancycrave1',
+    question: "How many bedrooms?",
   },
   {
     img: 'images/grid-list/hats-829509_640.jpg',
     title: 'Extra Amenities',
-    Status: 'Hans',
+    question: "There are a few other things...let's make it simple for you",
   },
   {
     img: 'images/grid-list/honey-823614_640.jpg',
     title: 'Pets',
-    Status: 'fancycravel',
+    question: " Pets are our best friends. Let us get to know if you have any little furry friends",
   },
  ];
 
 
-  export class Grid extends React.Component {
-    getChildContext() {
-      return { muiTheme: getMuiTheme(baseTheme) };
-    }
-    render() {
-      return (
-
-
+ export class Grid extends React.Component {
+   getChildContext() {
+     return { muiTheme: getMuiTheme(baseTheme) };
+   }
+   render() {
+     return (
   <div style={styles.root}>
     <GridList
       cellHeight={200}
       style={styles.gridList}
     >
-      <Subheader>December</Subheader>
+      <Subheader>"  Let's get to know a little bit about you"</Subheader>
       {tilesData.map((tile) => (
         <GridTile
           key={tile.img}
@@ -89,5 +87,6 @@ const tilesData = [
   Grid.childContextTypes = {
     muiTheme: React.PropTypes.object.isRequired,
   };
+
 
 export default Grid
