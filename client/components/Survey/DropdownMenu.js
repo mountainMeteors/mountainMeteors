7d54f4830018a907551dd9ddc075d4ec8ab76c2a
Jack <jisapp02@gmail.com>
2016-09-22 19:49:29 -0400

    Latest changes

--- client/components/Survey/DropdownMenu.js
+++ client/components/Survey/DropdownMenu.js
@@ -20,44 +20,53 @@ class DropdownMenu extends React.Component{
       super(props);
 
       this.state = {filter: ''}
-      this.getSelectedValue = this.getSelectedValue.bind(this);
-      this.onFormSubmit = this.onFormSubmit.bind(this);
+      // this.getSelectedValue = this.getSelectedValue.bind(this);
+      // this.onFormSubmit = this.onFormSubmit.bind(this);
 
     }
 
-    getSelectedValue (event,value) {
+    getSelectedValue (event) {
+      console.log('setting state to', event);
       this.setState({ filter:event.target.value });
     }
 
     onFormSubmit (event) {
       console.log('submitted', event);
       console.log('filter', this.state.filter);
-      // event.preventDefault();
-      this.props.postSurveyAnswers(this.state.filter);
-      this.setState({ filter: "" });
+      event.preventDefault();
+      // this.props.postSurveyAnswers(this.state.filter);
+      // this.setState({ filter: "" });
+
+
     }
 
 
+    getRef(ref){
+      console.log('getting ref', ref);
+      this.usernameRef = ref;
+    }
 
     render() {
+
       var message='You selected '+this.state.filter;
       var selection = this.state.filter
-      {console.log(selection)}
+      {console.log('rendered', selection)}
       return (
-        <div>
-         <select value={this.state.filter}
-          onChange={this.getSelectedValue} />
-            <option value="somewhere near MKS">somewhere near MKS</option>
-            <option value="MKS">MKS</option>
-            <option value="West Village">West Village</option>
-            <option value="East Village">East Village</option>
-          </select>
-          <p>{message}</p>
-              <button className="btn btn-primary" type="submit"
-              onSubmit={()=>this.onFormSubmit(this.state.filter)}>
-                Submit
-              </button>
-          </div>
+        <div className="col-sm-12">
+          <form onSubmit={() => this.onFormSubmit()}>
+            <div className="form-group col-sm-7">
+              <select ref={(ref) => this.getRef(ref)}>
+                 <option value="somewhere near MKS">somewhere near MKS</option>
+                 <option value="MKS">MKS</option>
+                 <option value="West Village">West Village</option>
+                 <option value="East Village">East Village</option>
+               </select>
+            </div>
+            <div className="form-group col-sm-5">
+              <button type="submit" className="btn btn-block btn-primary">Search Github</button>
+            </div>
+          </form>
+        </div>
       );
     }
 };
