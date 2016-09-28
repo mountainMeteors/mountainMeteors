e278efb13d89b0ec3104184b106fdc23f7cf13eb
wentran <wen.tran1@gmail.com>
2016-09-28 19:49:09 -0400

    postRankingsWorking

---
+++ client/actionCreators/rankingActions.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+
+export const POST_RANKINGS =  "POST_RANKINGS"
+export const FETCH_RANKINGS = 'FETCH_RANKINGS';
+
+export function postRankings({ amenities, commute, extras, neighborhood, pets, rent }){
+  console.log('======>>>>>>>>', "herereeee")
+  const surveyRankings = axios.post('/api/rankings', {
+    neighborhood, rent, pets, amenities, commute, extras
+  })
+  .then(function(response){
+    console.log('saved successfully')
+  })
+  return {
+    type: POST_RANKINGS,
+    payload: surveyRankings
+  }
+}
+
+
+
+export function fetchAnswers() {
+  const request = axios.get('api/getRankings');
+  console.log('+here++',request)
+
+  return {
+    type: FETCH_RANKINGS,
+    payload: request
+  };
+}
