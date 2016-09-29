3ec99b2356725dae5d49d414c437395bf52ec06c
wentran <wen.tran1@gmail.com>
2016-09-28 21:17:20 -0400

    getReq working , on render in display

--- client/reducers/rankings.js
+++ client/reducers/rankings.js
@@ -1,12 +1,12 @@
 import { FETCH_RANKINGS } from '../actionCreators/rankingActions';
 
-export default function(state=null, action){
+export default function(state=[], action){
   switch(action.type) {
     // console.log('action received', action)
     case FETCH_RANKINGS:
     console.log('in rankings redeucersss!!!', '++==', action.payload.data)
      return [
-      /* make a new array and put payload data in it and concat with state= state.concat([action.payload.data]);*/ action.payload.data, ...state]
+     state.concat([action.payload.data])]
   }
 
   return state;
