44681524d03df819e261a58010cb85afcfe2e8a1
Jack <jisapp02@gmail.com>
2016-09-30 16:19:29 -0400

    let to var

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -15,10 +15,10 @@ router.use((req, res, next) => {
 
 router.post('/signup', (req, res) => {
   console.log('server-side signup with', req.body);
-  let email = req.body.email;
-  let password = req.body.password;
+  var email = req.body.email;
+  var password = req.body.password;
 
-  // let salt = bcrypt.genSaltSync(10);
+  // var salt = bcrypt.genSaltSync(10);
   bcrypt.hash(password, null, null, function(err, hash) {
     console.log('hashing', hash);
     return db('users')
@@ -35,8 +35,8 @@ router.post('/signup', (req, res) => {
 router.post('/login', util.checkToken, (req, res) => {
   console.log('server-side login with', req.body);
   console.log('headers', req.headers);
-  let email = req.body.email;
-  let password = req.body.password;
+  var email = req.body.email;
+  var password = req.body.password;
 
   return db('users')
     .where({
