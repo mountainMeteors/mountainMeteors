a034aa0837d8e14866f63fb9f7f0938f1d5b1233
Jack <jisapp02@gmail.com>
2016-10-01 00:58:56 -0400

    Temporarily comment out add modal

--- client/actionCreators/listingActions.js
+++ client/actionCreators/listingActions.js
@@ -2,27 +2,14 @@ import axios from 'axios';
 
 export const GET_LISTINGS = 'GETLISTINGS';
 
-// export const getListings = function(user_id){
-//   const request = axios.get('/api/listings/' + user_id)
+export const getListings = function(user_id){
+  const request = axios.get('/api/listings/' + user_id)
   // .then(function (response) {
   //   console.log('listings response received', response.data);
   // })
   // .catch(function (error) {
   //   console.log(error);
   // });
-
-export const getListings = function(preference){
-  const request = axios.get('/api/listings' + user_id, {
-    location: preference.location,
-    price: preference.price,
-    pets: preference.pets
-  }).catch((response) => {
-    if(response instanceof Error){
-      console.error('Error sending response', response);
-    } else {
-      console.error('Error from server', response);
-    }
-  });
 
 
   return {
