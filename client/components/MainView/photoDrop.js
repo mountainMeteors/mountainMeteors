b8ca47a9b5286c7334f964a155d89023de781cef
wentran <wen.tran1@gmail.com>
2016-10-05 14:25:40 -0400

    photo uploading edited

--- client/components/MainView/photoDrop.js
+++ client/components/MainView/photoDrop.js
@@ -20,14 +20,30 @@ export default class photoDrop extends Component{
 
 
   render () {
-        return (
-            <div>
-              <Dropzone onDrop={this.onDrop}>
-                <div>Try dropping some files here, or click to select files to upload.</div>
-              </Dropzone>
-            </div>
+    const field = this.props;
+    const files = field.input.value;
+
+    return (
+      <div>
+      <Dropzone 
+      onDrop={( photos, event )=> {this.setState({
+        images: [...this.state.images, photos]}, function () {
+          field.input.onChange(this.state.images);
+        });
+    }
+  }
+    >
+
+    <div>Try dropping some files here, or click to select files to upload.</div>
+    </Dropzone>
+    {this.state.images.length > 0 ? <div>
+      <h2>  Uploading{this.state.images.length} photos...</h2>
+                <div id="imageContainer">{this.state.images.map((file) => <img key={file[0].name} className="imagePreview" src={file[0].preview} /> )}</div>
+             </div> : null}
+          </div>
         );
+        }
       }
 
-  }
+
 
