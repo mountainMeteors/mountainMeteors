2dcb40d21bced09ff72e03ac1ca05d219976fd3f
wentran <wen.tran1@gmail.com>
2016-10-05 12:27:23 -0400

    photo uploading in progress

--- client/components/MainView/PostPhotos.js
+++ client/components/MainView/PostPhotos.js
@@ -5,42 +5,8 @@ import { Field, reduxForm } from 'redux-form';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
 // import { createPost3 } from '../actions/actionCreators';
+import photoDrop from './photoDrop'
 
-class renderDropzoneInput extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: []
-    };
-  }
-  onSubmit(props) {
-    // this.props.createPost3(props)
-  }
-  render(){
-    // const { value, onChange } = this.props
-    const field = this.props
-    const files = field.input.value;
-
-  return (
-    <div>
-      <Dropzone
-        onDrop={( filesToUpload, e ) => {
-          this.setState({images: [...this.state.images,filesToUpload]}, function(){
-            field.input.onChange(this.state.images); //done in callback bc setState doesn't immediately mutate state
-          });
-        }
-      }
-      >
-        <div>Dropping photos here or select files to upload.</div>
-      </Dropzone>
-      {this.state.images.length > 0 ? <div>
-          <h2>Uploading {this.state.images.length} files...</h2>
-          <div id="imageContainer">{this.state.images.map((file) => <img key={file[0].name} className="imagePreview" src={file[0].preview} /> )}</div>
-       </div> : null}
-    </div>
-  );
-  }
-};
 
 
 
@@ -77,20 +43,11 @@ class PostPhotos extends Component{
               </div>
             </div>
 
-
-            <div>
-              <label htmlFor="DateView">Date View</label>
-              <Field name="DateView" component="input" type="text" className="form-control"/>
-              <div className="text-help">
-              </div>
-            </div>
-
-
             <div>
               <label htmlFor="images">Your future apartment ...</label>
               <Field
                 name="Photos"
-                component={renderDropzoneInput}
+                component={photoDrop}
               />
             </div>
             <button type="submit" className="btn btn-primary" id="buttonNew">Submit</button>
