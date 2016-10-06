faae4249574fe2e874cc7a44e02d4e913aece646
Yueh Chou <yueh.k.chou@gmail.com>
2016-10-06 16:42:02 -0400

    fix($compile): merge conflicts solved
    Files corrupted due to revert, removed corrupted files and replaced with most recent github files
    
    app crashes

--- client/components/MainView/Map.js
+++ client/components/MainView/Map.js
@@ -1,10 +1,50 @@
 import React from 'react';
-import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
+import {GoogleMapLoader, GoogleMap, Marker, InfoWindow} from "react-google-maps";
 
 class GoogMap extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+    this.handleMarkerClick = this.handleMarkerClick.bind(this);
+    this.handleCloseClick = this.handleCloseClick.bind(this);
   }
+
+  componentWillReceiveProps(props) {
+
+  }
+
+  handleMarkerClick(targetMarker) {
+    console.log('######TARGET MARKER#########', targetMarker)
+    this.setState({
+      markers: this.state.markers.map(marker => {
+        if (marker === targetMarker) {
+          return {
+            ...marker,
+            showInfo: true,
+          };
+        }
+        return marker;
+      }),
+    });
+  }
+
+  handleCloseClick(targetMarker) {
+    this.setState({
+      markers: this.state.markers.map(marker => {
+        if (marker === targetMarker) {
+          return {
+            ...marker,
+            showInfo: false,
+          };
+        }
+        return marker;
+      }),
+    });
+  }
+
 
   render() {
     return (
@@ -20,17 +60,27 @@ class GoogMap extends React.Component {
           }
           googleMapElement={
             <GoogleMap
-              ref={(map) => console.log(map)}
+              ref={(map) => console.log('map',map)}
               defaultZoom={15}
               defaultCenter={this.props.origin}
               onClick={this.props.onMapClick}
             >
-              {this.props.markers.map((marker, index) => {
-                return (
-                  <Marker
-                    {...marker}
-                    onRightclick={() => this.props.onMarkerRightclick(index)} />
-                );
+              {this.props.listings.map((marker, index) => {
+                const onClick = () => this.handleMarkerClick(marker);
+                const onCloseClick = () => this.handleCloseClick(marker);
+                if(!marker.archived){
+                  return (
+                    <Marker
+                    key={ index }
+                    onClick={ onClick }
+                    position={ {lat: marker.lat, lng: marker.lng}} >
+
+                      <InfoWindow onCloseClick={() => props.onMarkerClose(marker)}>
+                        <div>{marker.rent}</div>
+                      </InfoWindow>
+                    </Marker>
+                  );
+                }
               })}
             </GoogleMap>
           }
