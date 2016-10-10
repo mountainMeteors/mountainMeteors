2b6c4578e06fa4477b30697f1b568f0f1d4f107d
Yueh Chou <yueh.k.chou@gmail.com>
2016-10-09 23:43:00 -0400

    edit

--- server/routes/distRoutes.js
+++ server/routes/distRoutes.js
@@ -11,13 +11,26 @@ router.post('/dist', util.findDistance, function(req,res){
 var homeAddress, targetAddress
 router.get('/dist/:id', function(req,res) {
   console.log('###req.params.id###', req.params.id)
-  db('listings').where({
-    id: req.params.id
-  }).select('location')
+  db('listings').innerJoin('users')
+  .where({
+    id: req.params.id,
+    user_id: users.id
+  })
+  .select('location', 'prefs')
   .then(function(results){
     homeAddress = results
       res.send({'homeAddress': homeAddress })
     })
   })
+// router.get('/dist/:id', function(req,res) {
+//   console.log('###req.params.id###', req.params.id)
+//   db('listings').where({
+//     id: req.params.id
+//   }).select('location')
+//   .then(function(results){
+//     homeAddress = results
+//       res.send({'homeAddress': homeAddress })
+//     })
+//   })
 
 module.exports = router;
