9499694b7d4e00c40190e01ae79f2d16ed730624
wentran <wen.tran1@gmail.com>
2016-10-10 00:13:03 -0400

    listingEntrymapState

--- client/components/MainView/Listing.js
+++ client/components/MainView/Listing.js
@@ -183,7 +183,10 @@ class Listing extends React.Component{
 
 
 
+<<<<<<< abeaccc193dfef856182af18db0e1de37e237170
 
+=======
+>>>>>>> listingEntrymapState
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({putListing}, dispatch);
 }
