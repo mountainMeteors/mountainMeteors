17c3be8b1386e590b1a2598e12dd370d49bf745a
Yueh Chou <yueh.k.chou@gmail.com>
2016-10-09 22:09:56 -0400

    wip retireving target address from users db

--- server/routes/distRoutes.js
+++ server/routes/distRoutes.js
@@ -9,11 +9,9 @@ router.post('/dist', util.findDistance, function(req,res){
 })
 
 var homeAddress, targetAddress
-router.get('/dist/:id',function(req,res) {
+router.get('/dist/:id', function(req,res) {
   console.log('###req.params.id###', req.params.id)
   db('listings').where({
-    // user_id: req.user.id,
-
     id: req.params.id
   }).select('location')
   .then(function(results){
