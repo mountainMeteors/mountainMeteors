41c5850e7e6c13188fb4206b86614092e8a17a08
Yueh Chou <yueh.k.chou@gmail.com>
2016-11-02 16:28:13 -0400

    refactor($client/component/welcome): Fixed position of white column
    Changed the position of the dev page column from top to bottom

--- client/components/Welcome/Welcome.js
+++ client/components/Welcome/Welcome.js
@@ -26,14 +26,14 @@ class Welcome extends React.Component {
     const Email = 'assets/icons/email.png';
 
     const devList = person.map(function(item, index){
-        console.log("#####item#####", item)
+        console.log('#####item#####', item)
         return (
           <Col xs={6} md={3} key={index} padding={20}>
-            <image src= {item.img} className="profile"/>
-            <h2 className="devNames"> {item.name} </h2>
-            <h3 className="devTitles"> {item.role} </h3>
+            <image src= {item.img} className='profile'/>
+            <h2 className='devNames'> {item.name} </h2>
+            <h3 className='devTitles'> {item.role} </h3>
 
-            <div className="icons_container">
+            <div className='icons_container'>
               <a href={item.links.GitHub}><image src= {GitHub} className='icons' /></a>
               <a href={item.links.LinkedIn}><image src= {LinkedIn} className='icons' /></a>
               <a href={item.links.Email}><image src= {Email} className='icons' /></a>
@@ -43,8 +43,15 @@ class Welcome extends React.Component {
       })
 
     return (
-      <div className ="row" style={{padding:10}}>
-        {devList}
+      <div>
+      <video id="background-video" poster="https://dl.dropboxusercontent.com/s/nzmafm1x7d5gh2r/GettyImages-597661487.jpg?dl=0 " autoPlay loop>
+        <source src="http://dl.dropboxusercontent.com/s/gamzhby2es6n5zr/FlatironVidA.mp4?dl=0" type="video/mp4" />
+      </video>
+        <div className='devContainer'>
+          <div className="row" style={{padding:10}}>
+            {devList}
+          </div>
+        </div>
       </div>
     )
   }
